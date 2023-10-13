c33968aa9eb8299b10b3dac7d9cebe3b643f6da6
Anwar076 <121021733+Anwar076@users.noreply.github.com>
2023-10-13 16:57:22 +0200

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,7 +7,7 @@
 				<img src="{{ url($product->image ?? 'img/placeholder.jpg') }}" alt="{{ $product->title }}" class="rounded">
 				<div class="product-body">
 					<form action="{{ route('products.order', $product) }}" method="POST" data-controller="size">
-						<h5 class="product-title"><span>{{ $product->title }}</span><em>&euro;{{ $product->price }}</em></h5>
+						<h5 class="product-title"><span>{{ $product->title }}</span><em>&euro;{{ $product->getPriceWithDiscountAttribute() }}</em></h5>
 						@unless(empty($product->description))
 							<p>{{ $product->description }}</p>
 						@endunless
