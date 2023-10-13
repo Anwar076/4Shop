2b8c7394f9087be06031c8408300e1979f810923
Anwar076 <121021733+Anwar076@users.noreply.github.com>
2023-10-13 10:07:18 +0200

    Update Product.php
    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,15 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'price',
+        'discount',
+        'original_price', // Voeg dit veld toe
+        'leiding',
+        'active',
+        'category_id',
+    ];
+
+    public function getPriceWithDiscountAttribute()
+    {
+        if ($this->discount > 0) {
+            $discountedPrice = $this->price - ($this->price * ($this->discount / 100));
+            return number_format($discountedPrice, 2); // Als je de prijs wilt formatteren naar twee decimalen
+        }
+
+        return number_format($this->price, 2); // Als er geen korting is, retourneer de normale prijs
+    }
+
+    public function getPriceAttribute($value)
+    {
+        // Als er een kortingspercentage is, bereken de prijs na korting
+        if ($this->discount > 0) {
+            $discountedPrice = $value - ($value * ($this->discount / 100));
+            return number_format($discountedPrice, 2);
+        }
+
+        return number_format($value, 2);
+    }
+
+
+
     public function types()
     {
         return $this->hasMany(Type::class);
-    }
-    public function getPriceAttribute($value)
-    {
-        $discount = $value * ($this->discount / 100);
-        $final_price = $value - $discount;
-        return number_format($final_price, 2);
     }
 
     public function category()
