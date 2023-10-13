c080bd90f8daf165ac2982b707f730593af5483b
Anwar076 <121021733+Anwar076@users.noreply.github.com>
2023-10-13 10:07:05 +0200

    Update Order.php
    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,8 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $fillable = [
+        'name',
+        'email',
+        'speltak',
+        'amount',
+        'mollie_id',
+        'payed',
+        'delivered', // Voeg delivered toe aan de lijst van massaal toegewezen velden
+    ];
+
     public function rules()
-   	{
-   		return $this->hasMany(Order_rule::class);
-   	}
+    {
+        return $this->hasMany(Order_rule::class);
+    }
 }
