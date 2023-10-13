0997f2cc931acf819429ef58aecd130ec7a0db3c
Anwar076 <121021733+Anwar076@users.noreply.github.com>
2023-10-13 10:06:23 +0200

    Update ProductController.php
    Update ProductControIIer.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,15 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Category;
 use App\Models\Product;
-use App\Models\Type;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-
-
 class ProductController extends Controller
 {
-
     public function index()
     {
         $products = Product::all();
@@ -32,12 +28,11 @@ class ProductController extends Controller
             'price' => 'required|numeric',
             'active' => 'required|boolean',
             'leiding' => 'required|boolean',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Aanpasbaar aan je uploadvereisten
-            'description' => 'nullable|string|max:1000', // Aanpasbaar aan je vereisten
-            'category_id' => 'required|exists:categories,id', // Controleer of de geselecteerde categorie bestaat
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'description' => 'nullable|string|max:1000',
+            'category_id' => 'required|exists:categories,id',
         ]);
 
-        // Maak een nieuw product met de ingediende gegevens
         $product = new Product();
         $product->title = $request->input('title');
         $product->price = $request->input('price');
@@ -46,9 +41,8 @@ class ProductController extends Controller
         $product->description = $request->input('description');
         $product->category_id = $request->input('category_id');
 
-        // Upload en opslaan van de afbeelding (indien aanwezig)
         if ($request->hasFile('image')) {
-            $imagePath = $request->file('image')->store('public/images'); // Aanpasbaar aan je opslagvereisten
+            $imagePath = $request->file('image')->store('public/images');
             $product->image = str_replace('public/', '', $imagePath);
         }
 
@@ -57,19 +51,73 @@ class ProductController extends Controller
         return redirect()->route('admin.products.index')->with('success', 'Product is succesvol toegevoegd.');
     }
 
-
     public function edit(Product $product)
     {
-        return view('admin.products.edit', compact('product'));
+        // Haal de originele prijs op uit de database
+        $originalPrice = $product->price;
+
+        // Reset de kortingswaarde naar nul, zodat de oorspronkelijke prijs wordt weergegeven
+        $product->discount = 0;
+        $product->save();
+
+        $categories = Category::all();
+
+        // Bereken de prijs na korting
+        $discountedPrice = $product->getPriceWithDiscountAttribute();
+
+        // Passeer de originele prijs en de prijs na korting naar de weergave
+        return view('admin.products.edit', compact('product', 'categories', 'originalPrice', 'discountedPrice'));
     }
+
+
+
+
+
+
 
     public function update(Request $request, Product $product)
     {
-        // Validatie en update logica hier
+        $request->validate([
+            'title' => 'required',
+            'active' => 'required|boolean',
+            'leiding' => 'required|boolean',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'description' => 'nullable|string|max:1000',
+            'category_id' => 'required|exists:categories,id',
+            'discount' => 'nullable|numeric|min:0', // Behoud kortingspercentage-validatie
+        ]);
+
+        // Update de attributen van het product, behalve de prijs
+        $product->title = $request->input('title');
+        $product->active = $request->input('active');
+        $product->leiding = $request->input('leiding');
+        $product->description = $request->input('description');
+        $product->category_id = $request->input('category_id');
+        $product->discount = $request->input('discount');
+
+        // Bijwerken van het kortingspercentage (als het is ingediend)
+        if ($request->has('discount')) {
+            $product->discount = $request->input('discount');
+        } else {
+            // Als er geen kortingspercentage is ingediend, stel het in op null (optioneel)
+            $product->discount = null;
+        }
+
+        // Upload en opslaan van de afbeelding (indien aanwezig)
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('public/images');
+            $product->image = str_replace('public/', '', $imagePath);
+        }
+
+        $product->save();
+
+        return redirect()->route('admin.products.index')->with('success', 'Product is succesvol bijgewerkt.');
     }
+
 
     public function destroy(Product $product)
     {
-        // Verwijderen logica hier
+        $product->delete();
+        return redirect()->route('admin.products.index')->with('success', 'Product is succesvol verwijderd.');
     }
 }
